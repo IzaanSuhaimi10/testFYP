0be5a3d6c2b87e69c009b0f78cb383e7abe6eba6
Muhammad Iz'aan bin Suhaimi <94822573+IzaanSuhaimi10@users.noreply.github.com>
2026-01-04 12:05:47 +0800

    Delete manuhub (Iz'aan) directory

--- manuhub (Iz'aan)/app/views/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <div class="logo">
-        <a href="/manuhub/public">ManuHub</a>
-    </div>
-    <div class="nav-links">
-        <a href="/manuhub/public/index.php?action=login">Login</a>
-        <a href="/manuhub/public/index.php?action=register">Register</a>
-    </div>
-</header>
